cf1948c50ccf524fcdca88c3c4b53f464c0e168d
arvind singh <1141191arvind.singh@gamil.com>
2018-01-08 19:27:13 +0530

    changed function name and removed one method from strictParser

--- src/strictParseInfo.js
+++ src/strictParseInfo.js
@@ -2,35 +2,36 @@ const Parsed=require("./parsed.js");
 const ParseInfo=require("./parseInfo.js");
 const InvalidKeyError=require("./errors/invalidKeyError.js");
 
-const compareValidKeysCaseSens = function(list,key){
+const containsMatchCase = function(list,key){
   return list.find(function(validKey){
     return key==validKey;
   });
 }
 
-const compareValidKeysWithoutCaseSens = function(list,key){
+const containsIgnoreCase = function(list,key){
   return list.find(function(validKey){
     return key.toLowerCase()==validKey.toLowerCase();
   });
 }
 const getContains = function(caseSensitive){
   if(caseSensitive){
-    return compareValidKeysCaseSens
-  }else{
-    return compareValidKeysWithoutCaseSens;
+    return containsMatchCase;
   }
+  return containsIgnoreCase;
 }
+
+let contains = undefined;
 
 var StrictParseInfo=function(initialParsingFunction,validKeys,caseSensitive) {
   ParseInfo.call(this,initialParsingFunction);
   this.validKeys=validKeys;
-  this.contains = getContains(caseSensitive);
+  contains = getContains(caseSensitive);
 }
 
 
 StrictParseInfo.prototype=Object.create(ParseInfo.prototype);
 StrictParseInfo.prototype.pushKeyValuePair=function() {
-  if(!this.contains(this.validKeys,this.currentKey))
+  if(!contains(this.validKeys,this.currentKey))
     throw new InvalidKeyError("invalid key",this.currentKey,this.currentPos);
   this.parsedKeys[this.currentKey]=this.currentValue;
   this.resetKeysAndValues();
